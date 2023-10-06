d0101077f8b08b5b96fda2f8aa9f9ea48f465a0e
Jackson Smackson <crackman2@users.noreply.github.com>
2023-10-06 18:10:03 +0200

    1.0.4.4

--- update_handler.nim
+++ update_handler.nim
@@ -1,6 +1,6 @@
 import httpclient, configparser, os, irc
 
-let current_version* = "1.0.4.2"
+let current_version* = "1.0.4.4"
 
 var g_tmp_clean* = false
 
@@ -31,7 +31,8 @@ proc updt_check*(respond_to_caller:bool = false, iclient:Irc, ievent:IrcEvent):b
             
         if ini_version == current_version:
             echo " +-> version is up to date [",ini_version,"]"
-            iclient.privmsg(ievent.origin, "up to date (mine)[" & current_version & "] vs (online)[" & ini_version & "]")
+            if(respond_to_caller):
+                iclient.privmsg(ievent.origin, "up to date (mine)[" & current_version & "] vs (online)[" & ini_version & "]")
             return true
         elif(respond_to_caller):
             iclient.privmsg(ievent.origin, "attempting to update, cya")
