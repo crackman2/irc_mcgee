3810ec5884abcb07252da97d6e00fa8ba77f8143
crackman2 <none@all.com>
2023-10-06 13:25:41 +0200

    e

--- update_handler.nim
+++ update_handler.nim
@@ -1,4 +1,4 @@
-import httpclient, configparser,os
+import httpclient, configparser,os, osproc
 
 let current_version = "1.0.0"
 
@@ -23,12 +23,45 @@ proc updt_check*() =
             return
 
         echo " +-> update required. [",current_version,"] -> [", ini_version, "]"
+        
+        var
+            tmpdir = getTempDir() & "irc_mcgee\\"
+            tmpexe = tmpdir & "irc_mcupdate.exe"
 
-        if not dirExists(getTempDir() & "irc_mcgee"):
-            createDir(getTempDir() & "irc_mcgee")
+        try:
+            if not dirExists(tmpdir):
+                createDir(tmpdir)
+        except:
+            echo " +-> failed to create temp dir"
+            return
+        echo " +-> getting file"
 
+        try:
+            client.downloadFile("https://github.com/crackman2/irc-mcgee/raw/main/update/irc_mcgee.exe",tmpexe)
+        except:
+            echo " +-> downloading file failed"
+            return
+        
+        if fileExists(tmpexe):
+            echo " +-> download successful"
+            var tmpbat = tmpdir & "irc_mcpatch.bat"
+            writeFile(tmpbat,
+                "@echo off\n" &
+                "timeout /t 1 /nobreak > NUL\n" &
+                "del " & getCurrentDir() & getAppFilename() & "/f /q\n" &
+                "copy /Y " & tmpexe & " " & getCurrentDir() & getAppFilename() & " > NUL\n" &
+                getCurrentDir() & getAppFilename() & "\n" &
+                "del " & tmpdir & " /f /q\n"
+            )
 
-        removeDir(getTempDir() & "irc_mcgee")
+            discard execCmd("cmd.exe /C start /b " & tmpbat)
+
+            quit(0)
+            
+
+            
+
+        #removeDir(getTempDir() & "irc_mcgee")
     except:
         echo " +-> update failed"
     finally:
