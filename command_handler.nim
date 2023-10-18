ad2ce1add39879bdc37866342164a273684f8517
Jackson Smackson <crackman2@users.noreply.github.com>
2023-10-18 21:27:42 +0200

    1.1.0.0x - what?

--- command_handler.nim
+++ command_handler.nim
@@ -112,10 +112,10 @@ proc helper_responseHandler(event:IrcEvent, client:AsyncIrc, response:Future[Exe
                     if line.strip() != "":
                         value_filtered &= line & "\n"
 
-                # try:
-                #     value_filtered = convert(value_filtered, getCurrentEncoding(), "utf-8")
-                # except:
-                #     discard
+                try:
+                    value_filtered = convert(value_filtered, getCurrentEncoding(), "utf-8")
+                except:
+                    value_filtered = value_filtered
 
                 for line in value_filtered.splitLines():
                     if g_abort: break
